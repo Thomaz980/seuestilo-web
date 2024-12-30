b9d7773651c015f811bc46ccb61b0935c5d67b5e
jjamerson-mt <jjamersonmt@gmail.com>
2024-12-30 20:41:50 -0300

    mudado visualizacao de servicos e removido algumas funcionalidades temporariamente

--- src/lib/ServicoCard2.svelte
+++ src/lib/ServicoCard2.svelte
@@ -5,18 +5,8 @@
     export let link = '';
 </script>
 
-<a href={link || '/'} class="max-w-100 flex flex-col items-center gap-1 text-center">
-    <img src={image} alt="Product" class="max-w-60 ">
+<a href={link || '/'} class="max-w-72 flex flex-col items-center gap-1 text-center">
+    <img src={image} alt="Product" class="max-w-72">
     <span class="text-slate-700 text-lg">{title}</span>
     <span class="text-rose-400 text-xl">R$ {price}</span>
 </a>
-
-<style>
-
-    
-img {
-    max-height: 200px;
-    width: 200px;
-    object-fit: cover;
-}
-</style>
